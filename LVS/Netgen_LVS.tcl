88c0a6e9f7d0a0460025ad61f2f2f54658969f1d
rlc113 <46628160+rlc113@users.noreply.github.com>
2024-03-28 22:13:25 -0400

    Add files via upload

---
+++ LVS/Netgen_LVS.tcl
@@ -0,0 +1,2 @@
+lvs {/home/tutorial/openroad_script_files/OpenROAD-flow-scripts/flow/results/sky130hd/counter/base/LVS/counter.spice counter} {/home/tutorial/openroad_script_files/OpenROAD-flow-scripts/flow/results/sky130hd/counter/base/LVS/6_final.cdl counter} /usr/local/share/pdk/sky130A/libs.tech/netgen/sky130A_setup.tcl
+exit
