c2147500598b0f233e02e5339a601d339e57f49d
rlc113 <46628160+rlc113@users.noreply.github.com>
2024-03-28 21:52:42 -0400

    Add files via upload

---
+++ config/config.mk
@@ -0,0 +1,21 @@
+export DESIGN_NICKNAME = counter
+export DESIGN_NAME = counter
+export PLATFORM    = sky130hd
+
+export VERILOG_FILES = $(sort $(wildcard ./designs/src/$(DESIGN_NICKNAME)/*.v))
+export SDC_FILE      = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
+export PDN_TCL		 = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/pdn.tcl
+
+export PLACE_PINS_ARGS = -min_distance 4 -min_distance_in_tracks
+
+#export CORE_UTILIZATION = 20
+#export CORE_ASPECT_RATIO = 1
+#export CORE_MARGIN = 2
+
+export DIE_AREA   = 0 0 80 80
+export CORE_AREA  = 20 20 60 60
+
+export PLACE_DENSITY = 1
+export TNS_END_PERCENT = 100
+
+
