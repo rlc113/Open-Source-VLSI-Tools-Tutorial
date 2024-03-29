bd8ecf5fd0b3143be142b0fec28201924ff34863
rlc113 <46628160+rlc113@users.noreply.github.com>
2024-03-28 22:13:06 -0400

    Create Magic_Extraction.tcl

---
+++ LVS/Magic_Extraction.tcl
@@ -0,0 +1,18 @@
+# Read the GDS file
+gds read "/home/tutorial/openroad_script_files/OpenROAD-flow-scripts/flow/results/sky130hd/counter/base/6_final.gds"
+
+# Load the module that was just read
+load counter
+
+# Extract the module to an .ext file
+extract
+
+# Configure the ext2spice command for LVS
+ext2soice LVS
+
+# Extract the module to a SPICE file
+ext2spice counter
+
+# Terminate
+exit
+
